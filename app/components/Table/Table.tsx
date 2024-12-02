8cf7b0a085b2cbcd62d35148d0b8e180845cf4d3
Niklas Ek <ek.niklas@gmail.com>
2024-12-02 02:26:08 +0100

    table rows are links to product pages, styled

--- app/components/Table/Table.tsx
+++ app/components/Table/Table.tsx
@@ -1,6 +1,7 @@
 import classes from "./Table.module.css";
 import uidb from "../../public.json";
 import { useEffect, useState } from "react";
+import { Link } from "@remix-run/react";
 
 export default function Table({ search }: { search: string }) {
   const [done, setDone] = useState<boolean>(false);
@@ -9,50 +10,57 @@ export default function Table({ search }: { search: string }) {
     setDone(true);
   });
   return (
-    <table className={classes.myTable}>
-      <colgroup>
-        <col style={{ width: "max-content" }} />
-        <col style={{ width: "50%" }} />
-        <col style={{ width: "50%" }} />
-      </colgroup>
-      <thead>
-        <tr>
-          <th></th>
-          <th>Product Line</th>
-          <th>Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {uidb.devices
-          .filter((d) =>
-            d.product.name.toLowerCase().includes(search.toLowerCase())
-          )
-          .map((d, i) => (
-            <tr
-              key={d.id}
-              className={done ? "" : classes.transparent}
-              style={
-                {
-                  "--delay": `${i * 20 > 1000 ? 1000 : i * 20}ms`,
-                } as React.CSSProperties
-              }
-            >
-              <td>
-                <img
-                  src={`https://images.svc.ui.com/?u=https://static.ui.com/fingerpri
+    <div className={classes.tableWrapper}>
+      <table className={classes.myTable}>
+        <colgroup>
+          <col style={{ width: "max-content" }} />
+          <col style={{ width: "50%" }} />
+          <col style={{ width: "50%" }} />
+        </colgroup>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Product Line</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {uidb.devices
+            .filter((d) =>
+              d.product.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((d, i) => (
+              <tr
+                key={d.id}
+                className={done ? "" : classes.transparent}
+                style={
+                  {
+                    "--delay": `${i * 20 > 1000 ? 1000 : i * 20}ms`,
+                  } as React.CSSProperties
+                }
+                onClick={(e) => {
+                  console.log(e);
+                  (e.target as HTMLElement).querySelector("a")!.click();
+                }}
+              >
+                <td>
+                  <Link to={`products/${d.id}`} className={classes.link}></Link>
+                  <img
+                    src={`https://images.svc.ui.com/?u=https://static.ui.com/fingerpri
 nt/ui/images/${d.id}/default/${d.images.default}.png&w=${24}`}
-                  alt=""
-                  width={24}
-                  style={{
-                    viewTransitionName: i < 30 ? "img" + d.id : "initial",
-                  }}
-                />
-              </td>
-              <td>{d.line.name}</td>
-              <td>{d.product.name}</td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+                    alt=""
+                    width={24}
+                    style={{
+                      viewTransitionName: i < 30 ? "img" + d.id : "initial",
+                    }}
+                  />
+                </td>
+                <td>{d.line.name}</td>
+                <td>{d.product.name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
